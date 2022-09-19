5f8f3e2cf51c0059f25c018191c3241ed330bcbd
rbenavente <63422736+rbenavente@users.noreply.github.com>
2022-09-19 19:48:28 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "s3_bucket_private_repo" {
   bucket    = "abc"
+  acl       = var.acl
  }
  module "private_git_module" {
    source   = "git@github.com:rbenavente/private-module.git"
